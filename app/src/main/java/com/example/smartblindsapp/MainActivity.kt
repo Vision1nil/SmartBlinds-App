d24d8b67d84c056303a9f0935e57f566f8cba273
Vision1nil <harshit.agrawal2009@outlook.com>
2022-12-21 11:38:59 -0800

    got rid of useless button, saket requested me to commit again.

--- app/src/main/java/com/example/smartblindsapp/MainActivity.kt
+++ app/src/main/java/com/example/smartblindsapp/MainActivity.kt
@@ -1,16 +1,76 @@
 package com.example.smartblindsapp
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import com.example.smartblindsapp.R.*
+import com.example.smartblindsapp.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    // initializing the binding here. To use in other activty or fragments replace the "ActivityMainBinding"
+    // with "ActivityNameBinding" or "FragmentNameBinding"
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_main)
-        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+
+        // binding.root = the root view (typically the constraint or relative layout of the screen)
+        val mainView = binding.root
+        setContentView(mainView)
+
+        // when btn is clicked, load to the control activity.
+        binding.btnClickMe.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ControlActivity::class.java))
+            finish()
+        }
+
+
+    }
+
+    //  Making an HTTP API request to OpenWeatherMap to get weather data
+    private fun getWeatherData(cityLocation: String): String {
+
+
+        val city = cityLocation
+        val apiKey = "f37a62f535eae00acd40906fa7a3c3a7"
+        val apiURL = "https://api.openweathermap.org/data/2.5/weather?q=$city&mode=metric&units=imperial&appid=$apiKey"
+        val resultJSON = URL(apiURL).readText()
+
+        val jsonObj = JSONObject(resultJSON)
+
+        return jsonObj.getJSONArray("weather").getJSONObject(0).getString("main")
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
