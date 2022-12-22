8b8693544e20de61fda74a9bfa04f34040bcad48
Vision1nil <harshit.agrawal2009@outlook.com>
2022-12-22 09:06:59 -0800

    Oswald and some small changes

--- app/src/main/java/com/example/smartblindsapp/MainActivity.kt
+++ app/src/main/java/com/example/smartblindsapp/MainActivity.kt
@@ -33,21 +33,22 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     //  Making an HTTP API request to OpenWeatherMap to get weather data
-    private fun getWeatherData(cityLocation: String): String {
-
-
-        val city = cityLocation
-        val apiKey = "f37a62f535eae00acd40906fa7a3c3a7"
-        val apiURL = "https://api.openweathermap.org/data/2.5/weather?q=$city&mode=metric&units=imperial&appid=$apiKey"
-        val resultJSON = URL(apiURL).readText()
-
-        val jsonObj = JSONObject(resultJSON)
-
-        return jsonObj.getJSONArray("weather").getJSONObject(0).getString("main")
-
-
-    }
+//    private fun getWeatherData(cityLocation: String): String {
+//
+//
+//        val city = cityLocation
+//        val apiKey = "f37a62f535eae00acd40906fa7a3c3a7"
+//        val apiURL = "https://api.openweathermap.org/data/2.5/weather?q=$city&mode=metric&units=imperial&appid=$apiKey"
+//        val resultJSON = URL(apiURL).readText()
+//
+//        val jsonObj = JSONObject(resultJSON)
+//
+//        return jsonObj.getJSONArray("weather").getJSONObject(0).getString("main")
+//
+//
+//    }
 }
 
 
